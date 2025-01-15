7fe802d356d233b5be7ded1f79ce6cb06f2648a9
Nicolasurazanp <nicolasurazanp@gmail.com>
2025-01-14 22:34:09 -0500

    vista categorias

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
+import { Link } from 'react-router-dom';
 import '../styles/CategoriesPage.css';
+import IconoRestaurante from '../img/IconoResturante.jpeg';
+import IconoHospedaje from '../img/IconoHospedaje2.jpeg';
+import IconoEsparcimiento from '../img/IconoCeEspar.jpeg';
+import IconoVarios from '../img/IconoBares.jpeg';
+import FondoPage from '../img/iglesia3.jpg'; // Imagen de fondo
 
-const categories = ['Turismo', 'Gastronomía', 'Artesanías', 'Cultura', 'Naturaleza'];
+const categories = [
+  { name: 'Restaurantes', image: IconoRestaurante },
+  { name: 'Hospedaje', image: IconoHospedaje },
+  { name: 'Centros Esparcimiento', image: IconoEsparcimiento },
+  { name: 'Varios', image: IconoVarios },
+];
 
 const CategoriesPage = () => {
   return (
     <div className="categories-page">
-      <h1>CATEGORIAS</h1>
-      <div className="categories-container">
-        {categories.map((category, index) => (
-          <div className="category-card" key={index}>
-            <h2>{category}</h2>
-          </div>
-        ))}
-      </div>
-      <div className="back-button-container">
-        {/* Botón para volver a la página principal */}
-        <Link to="/" className="back-button">
-          Volver a la página principal
-        </Link>
+      {/* Fondo con filtro de desenfoque */}
+      <div
+        className="background"
+        style={{ backgroundImage: `url(${FondoPage})` }}
+      ></div>
+      <div className="overlay">
+        <h1 className="categoriasTitle">CATEGORÍAS</h1>
+        <div className="categories-container">
+          {categories.map((category, index) => (
+            <div
+              className="category-card"
+              key={index}
+              style={{ backgroundImage: `url(${category.image})` }}
+            >
+              <h2>{category.name}</h2>
+            </div>
+          ))}
+        </div>
+        <div className="back-button-container">
+          <Link to="/" className="back-button">
+            Volver a la página principal
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default CategoriesPage;
-
