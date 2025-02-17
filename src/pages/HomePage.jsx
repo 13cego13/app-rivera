30b8d60c688c0c226bbdaf4feb5d51f580d9a1ea
13cego13 <144627075+13cego13@users.noreply.github.com>
2025-02-17 08:46:37 -0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 import Carousel from '../components/Carousel';
 import '../styles/HomePage.css';
 
 const HomePage = () => {
+  const navigate = useNavigate(); // Inicializa useNavigate
+
   return (
     <div className="homepage">
       <Carousel />
       <div className="homepage-content">
-        <h1 className='titulo' >DESCUBRE RIVERA</h1>
-        <button className="explore-button" onClick={() => window.location.href = '/categories'}>
+        <h1 className="titulo">DESCUBRE RIVERA</h1>
+        <button
+          className="explore-button"
+          onClick={() => navigate('/categories')} // Usa navigate para cambiar de ruta
+        >
           EXPLORAR
         </button>
       </div>
